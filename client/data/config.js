426224ebcfad462fca0d170099e3ada972a16577
koladeore <koladeore@gmail.com>
2019-11-11 22:22:36 +0100

    error

--- client/data/config.js
+++ client/data/config.js
@@ -1,22 +1,9 @@
-import sentry from '@sentry/browser';
-import dotenv from 'dotenv';
+import * as Sentry from '@sentry/browser';
 
-dotenv.config();
-// const sentry_key = 'cb55d4f05cd443ce82303222f77ef5e0';
-// const sentry_app = '61499';
-// const Client_Id = {Client_Id:process.env.Client_Id}
-// const Client_Secret = {Client_Secret:SpeechRecognitionResultList.env.Client_Secret}
-// export const sentry_url = `https://${sentry_key}@app.getsentry.com/${sentry_app}`;
-
-
-// export function logException(ex, context) {
-//   Raven.captureException(ex, {
-//     extra: context
-//   });
-//   /*eslint no-console:0*/
-//   window && window.console && console.error && console.error(ex);
-// }
-sentry.init({
-  dsn: process.env.DSN_KEY
-})
-
+export function logException(ex, context) {
+  Sentry.captureException(ex, {
+    extra: context
+  });
+  /*eslint no-console:0*/
+  window && window.console && console.error && console.error(ex);
+}
--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -16,6 +16,19 @@ import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store, { history } from './store';
 
+// sentry
+import * as Sentry from '@sentry/browser';
+import { logException } from './data/config';
+
+Sentry.init({dsn: process.env.DSN_KEY});
+console.log(process.env.DSN_KEY);
+
+logException(new Error('Incomplete Data!'), {
+    email: 'koladeore@gmail.com'
+  });
+  
+Sentry.showReportDialog();
+
 const router = (
     <Provider store={store}>
         <Router history={history}>
